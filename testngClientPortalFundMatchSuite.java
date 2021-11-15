aa2e2586e21f4658a2013c2d8af761b4987c8e9f
adaigger <72876506+adaigger@users.noreply.github.com>
2021-11-15 10:51:29 -0500

    Update testngClientPortalFundMatchSuite.java

--- testngClientPortalFundMatchSuite.java
+++ testngClientPortalFundMatchSuite.java
@@ -7,12 +7,10 @@ import com.relevantcodes.extentreports.LogStatus;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.AssertJUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 import ibmarketing.automatedqa.mutualFunds.validations.testngToolFunctionalilty;
  
  
@@ -45,7 +43,7 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
  
     public WebElement GetCPTotalNum() {
         
-        return driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div/div/div[3]/div[2]/div[1]/div/div"));
+        return driver.findElement(By.xpath("/html/body/div[1]/main/section/div[2]/section/div/div[3]/div[2]/div[1]/div/div"));
      
     }
  
@@ -191,6 +189,136 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
  
              
         }
+        
+        public void CPFundsTotal_Advisor(int webTotal) throws InterruptedException {
+        	 
+            // Click on new column
+            driver.findElement(By.xpath("/html/body/div[3]/section[2]/div/div/div/div/div[1]/div/div[3]/div[2]/div[2]/table/tbody/tr[1]/td[1]")).click();
+            Thread.sleep(1000);
+            // Click on Login button
+            driver.findElement(By.xpath("/html/body/div[3]/section[2]/div/div/div/div/div[2]/div/div/div/p[1]/button")).click();
+            //Verify Login Page has loaded
+            Thread.sleep(3000);
+            //  String LoginURL = driver.getCurrentUrl();
+            //   System.out.println(LoginURL);
+ 
+            //Login into Client Portal
+            driver.findElement(By.xpath("//*[@id=\"user_name\"]")).sendKeys("davefa123");
+            driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("tester1");
+            driver.findElement(By.xpath("//*[@id=\"submitForm\"]")).click();
+ 
+            Thread.sleep(6000);
+ 
+            //Get the total number of funds returned from the search
+            String totalnum = GetCPTotalNum().getText();
+ 
+            String lastFourDigits = "";     //substring containing last 4 characters
+ 
+            if (totalnum.length() == 22)
+            {
+                lastFourDigits = totalnum.substring(totalnum.length() - 3);
+            }
+            else if (totalnum.length() == 25)
+            {
+                lastFourDigits = totalnum.substring(totalnum.length() - 6);
+            }
+            else if (totalnum.length() == 14)
+            {
+                lastFourDigits = totalnum.substring(totalnum.length() - 1);
+            }
+            else if (totalnum.length() > 2)
+            {
+                lastFourDigits = totalnum.substring(totalnum.length() - 2);
+            }
+            else
+            {
+                lastFourDigits = totalnum;
+            }
+ 
+            System.out.println("Total Funds from Tool: " + lastFourDigits);
+ 
+            String DeleteComma = lastFourDigits.replace(",", "");
+             
+            int Total = Integer.parseInt(DeleteComma);
+ 
+            if (Total == webTotal){
+                System.out.println("Total from CP " + lastFourDigits + " matches the website " + webTotal);
+                test.log(LogStatus.PASS, "Total from CP " + lastFourDigits + " matches the website " + webTotal);
+                
+            }
+ 
+            else{
+                System.out.println("Total from CP " + lastFourDigits + " does not match the website " + webTotal);
+                test.log(LogStatus.FAIL, "Total from CP " + lastFourDigits + " does not match the website " + webTotal);
+            }
+ 
+             
+        }
+        
+        public void CPFundsTotal_Broker(int webTotal) throws InterruptedException {
+       	 
+            // Click on new column
+            driver.findElement(By.xpath("/html/body/div[3]/section[2]/div/div/div/div/div[1]/div/div[3]/div[2]/div[2]/table/tbody/tr[1]/td[1]")).click();
+            Thread.sleep(1000);
+            // Click on Login button
+            driver.findElement(By.xpath("/html/body/div[3]/section[2]/div/div/div/div/div[2]/div/div/div/p[1]/button")).click();
+            //Verify Login Page has loaded
+            Thread.sleep(3000);
+            //  String LoginURL = driver.getCurrentUrl();
+            //   System.out.println(LoginURL);
+ 
+            //Login into Client Portal
+            driver.findElement(By.xpath("//*[@id=\"user_name\"]")).sendKeys("fully179");
+            driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("tester22");
+            driver.findElement(By.xpath("//*[@id=\"submitForm\"]")).click();
+ 
+            Thread.sleep(6000);
+ 
+            //Get the total number of funds returned from the search
+            String totalnum = GetCPTotalNum().getText();
+ 
+            String lastFourDigits = "";     //substring containing last 4 characters
+ 
+            if (totalnum.length() == 22)
+            {
+                lastFourDigits = totalnum.substring(totalnum.length() - 3);
+            }
+            else if (totalnum.length() == 25)
+            {
+                lastFourDigits = totalnum.substring(totalnum.length() - 6);
+            }
+            else if (totalnum.length() == 14)
+            {
+                lastFourDigits = totalnum.substring(totalnum.length() - 1);
+            }
+            else if (totalnum.length() > 2)
+            {
+                lastFourDigits = totalnum.substring(totalnum.length() - 2);
+            }
+            else
+            {
+                lastFourDigits = totalnum;
+            }
+ 
+            System.out.println("Total Funds from Tool: " + lastFourDigits);
+ 
+            String DeleteComma = lastFourDigits.replace(",", "");
+             
+            int Total = Integer.parseInt(DeleteComma);
+ 
+            if (Total == webTotal){
+                System.out.println("Total from CP " + lastFourDigits + " matches the website " + webTotal);
+                test.log(LogStatus.PASS, "Total from CP " + lastFourDigits + " matches the website " + webTotal);
+                
+            }
+ 
+            else{
+                System.out.println("Total from CP " + lastFourDigits + " does not match the website " + webTotal);
+                test.log(LogStatus.FAIL, "Total from CP " + lastFourDigits + " does not match the website " + webTotal);
+            }
+ 
+             
+        }
  
     @Test
     public void FundFamilySearch() throws InterruptedException {
@@ -198,6 +326,24 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
 		test = report.startTest(nameofCurrMethod);
         int webTotal = WebFundsTotal("", "United States", "Aberdeen Funds", "All", "All");
         CPFundsTotal(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void FundFamilySearch_Advisor() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "United States", "Aberdeen Funds", "All", "All");
+        CPFundsTotal_Advisor(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void FundFamilySearch_Broker() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "United States", "Aberdeen Funds", "All", "All");
+        CPFundsTotal_Advisor(webTotal);
         testngToolFunctionalilty.LogoutCP(driver);
     }
      
@@ -209,6 +355,24 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
         CPFundsTotal(webTotal);
         testngToolFunctionalilty.LogoutCP(driver);
     }
+    
+    @Test
+    public void DefaultSearch_Advisor() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "United States", "All", "All", "All");
+        CPFundsTotal_Advisor(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void DefaultSearch_Broker() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "United States", "All", "All", "All");
+        CPFundsTotal_Broker(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
      
     @Test
     public void CountrySearch() throws InterruptedException {
@@ -216,6 +380,24 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
 		test = report.startTest(nameofCurrMethod);
         int webTotal = WebFundsTotal("", "Germany", "All", "All", "All");
         CPFundsTotal(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void CountrySearch_Advisor() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "Germany", "All", "All", "All");
+        CPFundsTotal_Advisor(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void CountrySearch_Broker() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "Germany", "All", "All", "All");
+        CPFundsTotal_Broker(webTotal);
         testngToolFunctionalilty.LogoutCP(driver);
     }
      
@@ -227,6 +409,24 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
         CPFundsTotal(webTotal);
         testngToolFunctionalilty.LogoutCP(driver);
     }
+    
+    @Test
+    public void CountryFamilySearch_Advisor() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "Germany", "Allianz Global Funds", "All", "All");
+        CPFundsTotal_Advisor(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void CountryFamilySearch_Broker() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "Germany", "Allianz Global Funds", "All", "All");
+        CPFundsTotal_Broker(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
      
     @Test
     public void CommissionsSearch() throws InterruptedException {
@@ -234,6 +434,24 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
 		test = report.startTest(nameofCurrMethod);
         int webTotal = WebFundsTotal("", "United States", "All", "Yes", "All");
         CPFundsTotal(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void CommissionsSearch_Advisor() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "United States", "All", "Yes", "All");
+        CPFundsTotal_Advisor(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void CommissionsSearch_Broker() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "United States", "All", "Yes", "All");
+        CPFundsTotal_Broker(webTotal);
         testngToolFunctionalilty.LogoutCP(driver);
     }
      
@@ -245,6 +463,24 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
         CPFundsTotal(webTotal);
         testngToolFunctionalilty.LogoutCP(driver);
     }
+    
+    @Test
+    public void FundTypeSearch_Advisor() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "United States", "All", "All", "Stock");
+        CPFundsTotal_Advisor(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void FundTypeSearch_Broker() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("", "United States", "All", "All", "Stock");
+        CPFundsTotal_Broker(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
      
     @Test
     public void SymbolSearch() throws InterruptedException {
@@ -252,6 +488,24 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
 		test = report.startTest(nameofCurrMethod);
         int webTotal = WebFundsTotal("AGAZX", "United States", "All", "All", "All");
         CPFundsTotal(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void SymbolSearch_Advisor() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("AGAZX", "United States", "All", "All", "All");
+        CPFundsTotal_Advisor(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void SymbolSearch_Broker() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("AGAZX", "United States", "All", "All", "All");
+        CPFundsTotal_Broker(webTotal);
         testngToolFunctionalilty.LogoutCP(driver);
     }
      
@@ -263,6 +517,24 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
         CPFundsTotal(webTotal);
         testngToolFunctionalilty.LogoutCP(driver);
     }
+    
+    @Test
+    public void CUSIPSearch_Advisor() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("003020351", "United States", "All", "All", "All");
+        CPFundsTotal_Advisor(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void CUSIPSearch_Broker() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("003020351", "United States", "All", "All", "All");
+        CPFundsTotal_Broker(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
      
     @Test
     public void ISINSearch() throws InterruptedException {
@@ -270,6 +542,24 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
 		test = report.startTest(nameofCurrMethod);
         int webTotal = WebFundsTotal("US0030203287", "United States", "All", "All", "All");
         CPFundsTotal(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void ISINSearch_Advisor() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("US0030203287", "United States", "All", "All", "All");
+        CPFundsTotal_Advisor(webTotal);
+        testngToolFunctionalilty.LogoutCP(driver);
+    }
+    
+    @Test
+    public void ISINSearch_Broker() throws InterruptedException {
+    	String nameofCurrMethod = new Throwable().getStackTrace()[0].getMethodName();
+		test = report.startTest(nameofCurrMethod);
+        int webTotal = WebFundsTotal("US0030203287", "United States", "All", "All", "All");
+        CPFundsTotal_Broker(webTotal);
         testngToolFunctionalilty.LogoutCP(driver);
     }
      
@@ -290,5 +580,7 @@ public class testngClientPortalFundMatchSuite extends BaseTest {
             test.log(LogStatus.FAIL, "Total from search was not 0");
         }
     }
+    
+    
  
 }
